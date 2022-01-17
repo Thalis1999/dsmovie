28da5ba69c09567c905142df9271c36814eae71a
Thalis <occtober456@outlook.com>
2022-01-16 21:58:04 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class MovieController {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public MovieDTO findById(@PathVariableLong id) {
+	public MovieDTO findById(@PathVariable Long id) {
 		return service.findById(id);
 	}
 
